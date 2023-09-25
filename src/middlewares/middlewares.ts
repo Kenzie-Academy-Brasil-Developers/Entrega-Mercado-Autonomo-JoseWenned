35110581c917adabcd9f344611f77f8da4a6ef05
JoseWenned <118996462+JoseWenned@users.noreply.github.com>
2023-09-25 14:07:27 -0300

    Create isValidProductId

---
+++ src/middlewares/middlewares.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from "express";
+import { Market } from "../database";
+
+export const isValidProduct = (req: Request, res: Response, next: NextFunction) => {
+    
+    if(Market.some(product => product.name === req.body.name)){
+        return res.status(409).json({ error: "Product already registered." })
+    }
+
+    next()
+}
+
+export const isValidProductId = (req: Request, res: Response, next: NextFunction) => {
+
+    if(!Market.some(product => product.id === req.params.Id)){
+        return res.status(404).json({ error: "Product not found."})
+    }
+
+    next()
+}
